69227c1e1c3be3f508c688ec72d1b463ebb8b9ff
chernetsov mihail <mishavvvsweater@gmail.com>
2025-03-22 15:30:33 +0300

    update readme

---
+++ src/test/java/agroscience/notifications/NotificationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package agroscience.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
